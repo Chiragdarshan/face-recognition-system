de316aaafb7233c63ddba324c5206d94882bffb7
chiruchiragg <157567853+chiruchiragg@users.noreply.github.com>
2025-04-17 02:41:17 +0530

    intial commit

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ from tkinter import messagebox
 import tkinter.messagebox
 from time import strftime
 from datetime import datetime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, Flask!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 
 class Face_Recognition_System:
